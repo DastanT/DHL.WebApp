58dff1706c23597136e8a7eab70f1989faced134
Dastan <dastan.smarti@gmail.com>
2021-05-04 21:53:02 +0600

    Add project files.

---
+++ DHL.Courier.Model/Interface/exDeliveryBliz.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DHL.Courier.Model
+{
+    public interface exDeliveryBliz
+    {
+        int ContentId { get; set; }
+        string SchemeDeliver { get; set; }
+
+        void GetReceiverPhones();
+        void GetSenderPhones();
+    }
+}
